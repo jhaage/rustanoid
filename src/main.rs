6a2aa80161cddad7e7a10dab431ddb4c87e8f038
Jeremy Haage <jhaage@gmail.com>
2025-04-10 15:25:15 -0700

    Initial Player/paddle implementation with edge collision

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,56 @@
 use macroquad::prelude::*;
 
 const PLAYER_SIZE: Vec2 = Vec2::from_array([150f32, 40f32]);
+const PLAYER_SPEED: f32 = 700f32;
+
+struct Player {
+    rect: Rect,
+}
+
+impl Player {
+    pub fn new() -> Self {
+        Self {
+            rect: Rect::new(
+                screen_width() * 0.5f32 - PLAYER_SIZE.x*0.5f32,
+                screen_height() - 100f32,
+                PLAYER_SIZE.x,
+                PLAYER_SIZE.y,
+            ),
+        }
+    }
+
+    pub fn update(&mut self, dt: f32) {
+        let mut x_move = 0f32;
+        if is_key_down(KeyCode::Left) {
+            x_move -= 1f32;
+        }
+        if is_key_down(KeyCode::Right) {
+            x_move += 1f32;
+        }
+        self.rect.x += x_move * dt * PLAYER_SPEED;
+
+        if self.rect.x < 0f32 {
+            self.rect.x = 0f32;
+        }
+        if self.rect.x > screen_width() - self.rect.w {
+            self.rect.x = screen_width() - self.rect.w;
+        }
+
+    }
+
+    pub fn draw(&self) {
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, BLUE);
+    }
+}
 
 #[macroquad::main("rustanoid")]
 async fn main() {
-    let player_rect = Rect::new(
-        screen_width() * 0.5f32 - PLAYER_SIZE.x*0.5f32,
-        screen_height() - 100f32,
-        PLAYER_SIZE.x,
-        PLAYER_SIZE.y
-    );
+    let mut player = Player::new(); 
     
     loop {
+        player.update(get_frame_time());
         clear_background(WHITE);
-        draw_rectangle(player_rect.x, player_rect.y, player_rect.w, player_rect.h, DARKGRAY);
+        player.draw();
         next_frame().await
     }
 }
